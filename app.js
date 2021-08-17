28c2e8bac0373e3404fcf65133aea521397b2df2
Michael Rider <mikepdxrider@gmail.com>
2021-08-16 17:36:25 -0700

    stretch goals phase 1

--- app.js
+++ app.js
@@ -3,23 +3,25 @@ import { countsAsAYes } from './utils.js';
 
 const quizButtonEl = document.getElementById('quiz-button');
 const resultDiv = document.getElementById('result-div');
-// const attemptCounterEl = document.getElementById('attempt-counter');
-// const passCounterEl = document.getElementById('pass-counter');
-// const failCounterEl = document.getElementById('fail-counter');
+const attemptCounterEl = document.getElementById('attempt-counter');
+const passCounterEl = document.getElementById('pass-counter');
+const failCounterEl = document.getElementById('fail-counter');
 
 // initialize state
-// let attemptCounter = 0;
-// let passCounter = 0;
-// let failCounter = 0;
+let attemptCounter = 0;
+let passCounter = 0;
+let failCounter = 0;
 
 // set event listeners 
-quizButtonEl.addEventListener('click', () =>{
-    // get user input
+quizButtonEl.addEventListener('click', () =>{  
+  // get user input
     const firstName = prompt('What\'s your first name?');
     const lastName = prompt('What\'s your last name?');
-    const userConfirmation = confirm('Are you ready for the quiz?');
+    const userConfirmation = confirm(`Thanks ${firstName} ${lastName}! Are you ready for the quiz?`);
+
     if (userConfirmation) {
         let correctAnswer = 0;
+        attemptCounter++;
         const q1 = prompt('Honey wine was produced in ancient times?');
         if (countsAsAYes(q1)) correctAnswer++;
         const q2 = prompt('Mead is created by fermenting honey with water?');
@@ -27,8 +29,20 @@ quizButtonEl.addEventListener('click', () =>{
         const q3 = prompt('Mead is the product of fermented berries or certain other fruits?');
         if (!countsAsAYes(q3)) correctAnswer++;
         alert('Quiz complete! Results are printing to page!');
-        // PROBLEM: ${} is not working as expected.
         resultDiv.textContent = `Okay ${firstName} ${lastName}, you got ${correctAnswer}/3 correct.`;
+        if (correctAnswer === 3) {
+            passCounter++ ;
+            resultDiv.style.color = 'green';
+            resultDiv.textContent += 'Great work! You got 100% of the questions right.';
+        } else {
+            failCounter++;
+            let percentage = Math.floor((correctAnswer / 3) * 100);
+            resultDiv.style.color = 'red';
+            resultDiv.textContent += `You only got ${percentage}% of the questions right.`;
+        }
+        attemptCounterEl.textContent = `Attempts: ${attemptCounter}`;
+        passCounterEl.textContent = `Pass: ${passCounter}`;
+        failCounterEl.textContent = `Fail: ${failCounter}`;
     } else {
         return;
     }
--- test/example.test.js
+++ test/example.test.js
@@ -3,7 +3,7 @@ import { countsAsAYes } from '../utils.js';
 
 const test = QUnit.test; 
 
-test('Input Yes expecting yes', (expect) => {
+test('Input Yes expecting True', (expect) => {
     //Arrange
     // Set up your arguments and expectations
     const expected = true;
@@ -11,6 +11,20 @@ test('Input Yes expecting yes', (expect) => {
     //Act 
     // Call the function you're testing and set the result to a const
     const actual = countsAsAYes('Yes');
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
+test('Input No expecting False', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = false;
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = countsAsAYes('No');
 
     //Expect
     // Make assertions about what is expected versus the actual result
